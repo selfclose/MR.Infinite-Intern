e5fd64dd197f21a5287663fb360f355a17717bee
Arnun <it_531413016@hotmail.com>
2017-01-01 13:09:38 +0700

    update name trait to use json

--- wp-content/plugins/Internships/src/Intern/ConcatTrait/NameLangTrait.php
+++ wp-content/plugins/Internships/src/Intern/ConcatTrait/NameLangTrait.php
@@ -4,22 +4,29 @@ namespace Intern\ConcatTrait;
 
 trait NameLangTrait
 {
-    protected $name;
+    /* default is th */
+    protected $name = '[]';
 
-
-    /**
-     * @return string
-     */
-    public function getName($lang = 'th')
-    {
-        return $this['name_'.$lang];
+    function addName($name, $lang = 'th') {
+        $n = json_decode($this->name, true);
+        $n = array_merge($n, [$lang => $name]);
+        $this->name = json_encode($n);
     }
 
-    /**
-     * @param string $name
-     */
-    public function setName($name, $lang = 'th')
-    {
-        $this['name_'.$lang] = $name;
+    function removeName($lang) {
+        $n = json_decode($this->name, true);
+        unset($n[$lang]);
+        $this->name = json_encode($n);
+    }
+
+    public function getName($lang = 'th') {
+        return json_decode($this->name, true)[$lang];
+    }
+
+    function setName($name_arr) {
+        //check if array is 1 dimensional
+        if (count($name_arr) == count($name_arr, COUNT_RECURSIVE)) {
+            $this->name = json_encode($name_arr);
+        }
     }
 }
